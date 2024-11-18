4b4cedb67ddfc34f49461bbb22d2589d269035b9
Gabo Esquivel <contact@gaboesquivel.com>
2024-11-18 10:25:26 -0600

    fix(hooks): take arguments

--- packages/opyn-hooks/controller/index.ts
+++ packages/opyn-hooks/controller/index.ts
@@ -1,14 +1,15 @@
 import { OpynAbis, OpynAddresses } from '@opyn/core'
 import { useWriteContract } from 'wagmi'
 
-export function useRedeem() {
+export function useExecute() {
   const { writeContract, ...exports } = useWriteContract()
 
-  const execute = () => {
+  const execute = (args: unknown[]) => {
     writeContract({
       address: OpynAddresses.Controller,
       abi: OpynAbis.Controller,
       functionName: 'execute',
+      args,
     })
   }
 
--- packages/opyn-hooks/redemption/index.ts
+++ packages/opyn-hooks/redemption/index.ts
@@ -4,11 +4,12 @@ import { useWriteContract } from 'wagmi'
 export function useRedeem() {
   const { writeContract, ...exports } = useWriteContract()
 
-  const redeem = () => {
+  const redeem = (args: unknown[]) => {
     writeContract({
       address: OpynAddresses.Redemption,
       abi: OpynAbis.Redemption,
       functionName: 'redeem',
+      args,
     })
   }
 
