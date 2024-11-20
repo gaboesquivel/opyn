976351c8d1debe80af429f947e44caf0503a4ed1
Gabo Esquivel <contact@gaboesquivel.com>
2024-11-20 07:59:51 -0600

    chore(web): add arbitrum sepolia

--- apps/web/components/layout/providers.tsx
+++ apps/web/components/layout/providers.tsx
@@ -15,7 +15,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { merge } from 'lodash'
 import type { ThemeProviderProps } from 'next-themes/dist/types'
 import { WagmiProvider } from 'wagmi'
-import { arbitrum } from 'wagmi/chains'
+import { arbitrum, arbitrumSepolia } from 'wagmi/chains'
 
 const queryClient = new QueryClient()
 
@@ -28,7 +28,7 @@ export const wagmiConfig = getDefaultConfig({
       wallets: [metaMaskWallet, trustWallet, walletConnectWallet],
     },
   ],
-  chains: [arbitrum],
+  chains: [arbitrum, arbitrumSepolia],
 })
 
 const customRainbowKitTheme: RainbowKitTheme = merge(darkTheme(), {
